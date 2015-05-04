dad4d2f586916f6e3876a81bb5d470862de14964
Huy Quan <anhhuydng@gmail.com>
2015-05-04 22:21:22 +0700

    Complete code

--- BookStore.Site/app/components/home/homeController.js
+++ BookStore.Site/app/components/home/homeController.js
@@ -2,31 +2,11 @@
 
 myApp.controller('homeController', ['$scope', '$routeParams', 'homeService', function ($scope, routeParams, homeService) {
 
-    //var value = routeParams.value;
-    //if (value === undefined || value < 0) value = 0;
-
     $scope.page = 0;
 
     homeService.getLatestBook().then(function (latestBook) {
         $scope.latestBook = latestBook.data;
     });
-
-    //homeService.getBookByPage(value).then(function (books) {
-    //    $scope.bookByPage = books.data;
-    //    $scope.showPrev = function () {
-    //        if (value === undefined || value === '0')
-    //            return false;
-    //        else
-    //            return true;
-    //    }
-
-    //    $scope.showNext = function () {
-    //        if ($scope.bookByPage.length < 5)
-    //            return false;
-    //        else
-    //            return true;
-    //    }
-    //});
 
     $scope.getBookByPage = function (value) {
         homeService.getBookByPage(value).then(function (books) {
